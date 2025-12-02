7a55ad92c5bfd2cad6c1d76f670a1029ecf0a70e
eangao <elmarcera@gmail.com>
2025-12-02 17:22:44 +0800

    Fetch User Profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,6 +5,16 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { profileSchema } from "./schemas";
+import { get } from "http";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this routes");
+
+  if (!user.privateMetadata?.hasProfile) redirect("/profile/create");
+
+  return user;
+};
 
 export const createProfileAction = async (
   prevState: any,
@@ -49,4 +59,23 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage || null;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: { clerkId: user.id },
+  });
+
+  if (!profile) redirect("/profile/create");
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "update profile action not implemented yet" };
 };
