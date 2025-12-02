f84f6659e1a881f441bc7233a1b587018c7b14bd
eangao <elmarcera@gmail.com>
2025-12-02 16:27:47 +0800

    Fetch Profile Image Action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,20 @@
-import { LuUser } from 'react-icons/lu';
+import { LuUser } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
 
-function UserIcon() {
-  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage) {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element
+      <img
+        src={profileImage}
+        alt="User Profile"
+        className="w-6 h-6 bg-primary rounded-full text-white object-cover"
+      />
+    );
+  }
+
+  return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
