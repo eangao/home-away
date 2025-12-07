f2485302ab971196bb20a37a480f7e7673d3e195
eangao <elmarcera@gmail.com>
2025-12-07 18:32:48 +0800

    Create Property Page - Setup

---
+++ app/rentals/create/page.tsx
@@ -0,0 +1,38 @@
+import FormInput from "@/components/form/FormInput";
+import FormContainer from "@/components/form/FormContainer";
+import { createPropertyAction } from "@/utils/actions";
+import { SubmitButton } from "@/components/form/Buttons";
+
+function CreatePropertyPage() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-8">Create Property</h1>
+
+      <div className="border p-8 rounded">
+        <h3 className="text-lg font-medium mb-4">General Info</h3>
+        <FormContainer action={createPropertyAction}>
+          <div className="grid md:grid-cols-2 gap-8 mb-4">
+            <FormInput
+              name="name"
+              type="text"
+              label="Name (20 limit)"
+              defaultValue="Cabin in Lativa"
+            />
+            <FormInput
+              name="tagLine"
+              type="text"
+              label="Tagline (30 limit)"
+              defaultValue="Dream Gateway Awaits You Here"
+            />
+            {/* price */}
+            {/* categories */}
+          </div>
+          {/* text area / description */}
+          <SubmitButton text="create rental" className="mt-12" />
+        </FormContainer>
+      </div>
+    </section>
+  );
+}
+
+export default CreatePropertyPage;
--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,12 @@ import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { imageSchema, profileSchema, validateWithZodSchema } from "./schemas";
+import {
+  imageSchema,
+  profileSchema,
+  propertySchema,
+  validateWithZodSchema,
+} from "./schemas";
 import { uploadImage } from "./supabase";
 
 const getAuthUser = async () => {
@@ -118,4 +123,21 @@ export const updateProfileImageAction = async (
   } catch (error) {
     return renderError(error);
   }
+};
+
+export const createPropertyAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(propertySchema, rawData);
+
+    return { message: "Property created successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+
+  redirect("/");
 };
