4ddc9bdc524decd51792a1f2a970d8776fb6d4c4
eangao <elmarcera@gmail.com>
2025-12-09 16:15:14 +0800

    Favorites Page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,9 +1,14 @@
-function FavoritesPage() {
-    return (
-        <div>
-            <h1>Favorites</h1>
-        </div>
-    )
-}
+import EmptyList from "@/components/home/EmptyList";
+import PropertiesList from "@/components/home/PropertiesList";
+import { fetchFavorites } from "@/utils/actions";
 
+async function FavoritesPage() {
+  const favorites = await fetchFavorites();
+
+  if (favorites.length === 0) {
+    return <EmptyList />;
+  }
+
+  return <PropertiesList properties={favorites} />;
+}
 export default FavoritesPage;
