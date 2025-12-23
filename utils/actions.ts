7d4d92f9f12ae6f0613dd801a29214c28c7acecf
Elmar Angao <elmarcera@gmail.com>
2025-12-23 14:00:16 +0700

    Fetch Bookings and Delete Booking

--- utils/actions.ts
+++ utils/actions.ts
@@ -386,7 +386,6 @@ export const findExistingReview = async (
   });
 };
 
-
 export const createBookingAction = async (prevState: {
   propertyId: string;
   checkIn: Date;
@@ -423,4 +422,41 @@ export const createBookingAction = async (prevState: {
     return renderError(error);
   }
   redirect("/bookings");
+};
+
+export const fetchBookings = async () => {
+  const user = await getAuthUser();
+  const bookings = await db.booking.findMany({
+    where: {
+      profileId: user.id,
+    },
+    include: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          country: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return bookings;
+};
+
+export const deleteBookingAction = async (prevState: { bookingId: string }) => {
+  const { bookingId } = prevState;
+  const user = await getAuthUser();
+  try {
+    await db.booking.delete({
+      where: { id: bookingId, profileId: user.id },
+    });
+
+    revalidatePath("/bookings");
+    return { message: "Booking deleted successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
